5d4ae4137fadb2419bf5dd3eb6982c5e360b105e
Diego Martignoni <diego.martignoni@lemonpie.it>
2024-08-27 12:06:19 +0200

    Add support for help command in command line arguments

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const inputString = process.argv[2];
 
 let template;
 // Verifica se è stato specificato il campo "--template" nella console app
-if (process.argv.includes("--template" || "-t")) {
+if (process.argv.includes("--template") || process.argv.includes("-t")) {
     const templateIndex = process.argv.indexOf("--template");
     template = process.argv[templateIndex + 1];
+}
+
+// Verifica se è stato specificato il comando "--help" nella console app
+if (process.argv.includes("--help") || process.argv.includes("-h")) {
+    console.log("Comandi disponibili:");
+    console.log("--template, -t: Specifica il template da utilizzare");
+    console.log("--help, -h: Mostra l'elenco dei comandi disponibili");
+    return;
 }
 
 // Verifica il formato della stringa di input
