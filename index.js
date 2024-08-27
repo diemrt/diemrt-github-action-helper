c4810d9b0beb97c95d799620778fc212f506cd50
Diego Martignoni <diego.martignoni@lemonpie.it>
2024-08-27 12:01:47 +0200

    Add support for template flag in command line arguments

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const inputString = process.argv[2];
-const template = process.argv[4];
+
+let template;
+// Verifica se è stato specificato il campo "--template" nella console app
+if (process.argv.includes("--template" || "-t")) {
+    const templateIndex = process.argv.indexOf("--template");
+    template = process.argv[templateIndex + 1];
+}
 
 // Verifica il formato della stringa di input
 const regex = /https:\/\/(\w+)-(\w+)-(\d+)/g;
@@ -14,7 +20,6 @@ const [, firstPart, secondPart, thirdPart] = match;
 
 switch (template) {
     case "yaml":
-
         console.log(`azure-static-web-apps-${firstPart}-${secondPart}-${thirdPart}.yml`);
         break;
 
